48bbbb062e162e748456cc6175f677ccb6eebbf9
irakli chanuyvadze <iraklichanuyvadze@gmail.com>
2024-08-03 19:18:14 +0400

    add basic test cases

--- src/ui/components/Toast/Toast.tsx
+++ src/ui/components/Toast/Toast.tsx
@@ -3,6 +3,7 @@ import { ToastInput } from '@/providers/toast/types';
 export function Toast({ type, body }: ToastInput) {
   return (
     <div
+      data-testid='toast'
       className={`absolute bottom-5 right-5 bg-red-300 animate-fadeIn px-6 py-4 rounded-lg text-white  ${type === 'success' ? 'bg-green-500' : 'bg-red-500'}`}>
       {body}
     </div>
--- src/ui/components/Toast/toast.spec.tsx
+++ src/ui/components/Toast/toast.spec.tsx
@@ -1,5 +1,41 @@
-describe('<Toast />', () => {
-  it('renders the component', () => {
-    expect(1).toEqual(1);
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { Toast } from './Toast';
+
+describe('Toast Component', () => {
+  it('renders with the correct message', () => {
+    const message = 'This is a toast message';
+    const { getByText } = render(<Toast type='success' body={message} />);
+    expect(getByText(message)).toBeInTheDocument();
+  });
+
+  it('applies the correct class fosr success type', () => {
+    const message = 'Success message';
+    const { getByTestId } = render(<Toast type='success' body={message} />);
+    const toast = getByTestId('toast');
+    expect(toast).toHaveClass('bg-green-500');
+  });
+
+  it('applies the correct class for error type', () => {
+    const message = 'Error message';
+    const { getByTestId } = render(<Toast type='error' body={message} />);
+    const toast = getByTestId('toast');
+    expect(toast).toHaveClass('bg-red-500');
+  });
+
+  it('has the correct animation class', () => {
+    const message = 'Animated message';
+    const { getByTestId } = render(<Toast type='success' body={message} />);
+    const toast = getByTestId('toast');
+    expect(toast).toHaveClass('animate-fadeIn');
+  });
+
+  it('is positioned correctly', () => {
+    const message = 'Position test message';
+    const { getByTestId } = render(<Toast type='success' body={message} />);
+    const toast = getByTestId('toast');
+    expect(toast).toHaveClass('absolute bottom-5 right-5');
   });
 });
